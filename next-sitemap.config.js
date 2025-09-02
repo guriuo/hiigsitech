6bbc0b2917acb0364c66eb687c585f1ef565783f
Mubarik Osman <zamier372@gmail.com>
2025-09-02 09:44:23 +0300

    Add new course page

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hiigsitech.com',
+  generateRobotsTxt: true, // also creates robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
+};
